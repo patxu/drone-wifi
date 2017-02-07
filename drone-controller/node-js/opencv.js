102a088097811c4fd2ce5d05f8a889227e534521
patxu <patrickxu13@gmail.com>
2017-02-07 14:01:18 -0500

    working video stream

--- drone-controller/node-js/opencv.js
+++ drone-controller/node-js/opencv.js
@@ -28,21 +28,17 @@ var server = http.createServer(function(req, res) {
       if (err) throw err;
       if (im.width() < 1 || im.height() < 1) throw new Error('Image has no size');
 
-      im.detectObject("haarcascade_frontalface_alt.xml", {}, function(err, faces){
+      im.detectObject("data/haarcascade_frontalface_alt.xml", {}, function(err, faces){
         if (err) throw err;
 
         for (var i = 0; i < faces.length; i++){
           var face = faces[i];
-          im.ellipse(face.x + face.width / 2, face.y + face.height / 2, face.width / 2, face.height / 2);
+          im.ellipse(face.x + face.width / 2, face.y + face.height / 2,
+             face.width / 2, face.height / 2);
         }
 
-        im.save('./tmp/face-detection.png');
-
-        fs.readFile('./tmp/face-detection.png', function(err, data) {
-
-          res.write('--daboundary\nContent-Type: image/png\nContent-length: ' + data.length + '\n\n');
-          res.write(data);
-        });
+      res.write('--daboundary\nContent-Type: image/png\nContent-length: ' + im.toBuffer().length + '\n\n');
+      res.write(im.toBuffer());
 
       });
     });
